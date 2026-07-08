adce404afcfe26170f60ce9ac194ce06bbb38a5d
SrijanJoshi-03 <srijanjoshi552@gmail.com>
2026-07-08 20:36:48 +1000

    Remove backend.tf and variable.tf from tracking

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket       = "remote-backend-for-terraform-statefile-practice-2026"
-    key          = "project/terraform"
-    region       = "us-east-1"
-    use_lockfile = true
-    encrypt      = false
-  }
-}
